08ec937029b8fcf33abb3b3accc4f92d46892822
andy <andy.pandaan@outlook.com>
2026-01-04 18:41:10 +0400

    changed vim  linenumber to a different column

--- modules/editors/vim/ui.nix
+++ modules/editors/vim/ui.nix
@@ -1,4 +1,3 @@
-# Vim UI configuration (theme, statusline, etc.)
 ''
   " ============================================================================
   " Theme
@@ -7,10 +6,22 @@
   let g:tokyonight_style = 'night'
   let g:tokyonight_enable_italic = 1
   let g:tokyonight_transparent_background = 1
+
   colorscheme tokyonight
 
-  highlight LineNr guifg=#e0af68 ctermfg=180
-  highlight CursorLineNr guifg=#ff9e64 gui=bold cterm=bold ctermfg=215
+  " ============================================================================
+  " Line number colors (post-colorscheme, persistent)
+  " ============================================================================
+  augroup VimLineNumberColors
+    autocmd!
+    autocmd ColorScheme * highlight LineNr guifg=#5e8ed1 guibg=NONE ctermfg=60 ctermbg=NONE
+    autocmd ColorScheme * highlight CursorLineNr guifg=#89ddff gui=bold guibg=NONE cterm=bold ctermfg=117 ctermbg=NONE
+    autocmd ColorScheme * highlight CursorLine guibg=NONE ctermbg=NONE
+  augroup END
+
+  set number
+  set relativenumber
+  set cursorline
 
   " ============================================================================
   " Lightline
@@ -25,11 +36,12 @@
     \              [ 'fileformat', 'fileencoding', 'filetype' ] ]
     \ },
     \ }
+
   set laststatus=2
   set noshowmode
 
   " ============================================================================
-  " GitGutter
+  " GitGutter (symbols only)
   " ============================================================================
   let g:gitgutter_sign_added = '+'
   let g:gitgutter_sign_modified = '~'
@@ -37,3 +49,4 @@
   let g:gitgutter_sign_removed_first_line = '^'
   let g:gitgutter_sign_modified_removed = '~'
 ''
+
